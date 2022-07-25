10e4e1e9025ccbfa311548d542e8c55f01566d33
Infiinite-lens <okorelens27@gmail.com>
2022-07-25 15:36:16 +0300

    Images added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import imagelist from "./assets/images";
+import "./App.scss";
 
 function App() {
+  const randomLength = () => {
+    let y = Math.floor(Math.random() * 4 + 1);
+    // console.log(y);
+    return y;
+  };
+  const randomWidth = () => {
+    let x = Math.floor(Math.random() * 4 + 1);
+    // console.log(x);
+    return x;
+  };
+
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Image Gallery</h2>
+      <div className="container">
+        {imagelist.map((image) => (
+          <div
+            key={image.id}
+            className={`gallery-container w-${randomWidth()} h-${randomLength()}`}
+          >
+            <div className="gallery-item">
+              <div onClick={clickHandler} className="image">
+                <img src={image.src} alt={image.text} />
+              </div>
+              <div className="text">{image.text}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/assets/images.js
@@ -0,0 +1,130 @@
+const imagelist = [
+  {
+    src: "https://source.unsplash.com/1600x900/?nature",
+    text: "nature",
+    id: 1,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?sports",
+    text: "sports",
+    id: 2,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?textures-patterns",
+    text: "textures-patterns",
+    id: 3,
+  },
+  { src: "https://source.unsplash.com/1600x900/?cars", text: "cars", id: 4 },
+  {
+    src: "https://source.unsplash.com/1600x900/?experimental",
+    text: "experimental",
+    id: 5,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?people",
+    text: "people",
+    id: 6,
+  },
+  { src: "https://source.unsplash.com/1600x900/?food", text: "food", id: 7 },
+  {
+    src: "https://source.unsplash.com/1600x900/?architecture",
+    text: "architecture",
+    id: 8,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?business-work",
+    text: "business-work",
+    id: 9,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?fashion",
+    text: "fashion",
+    id: 10,
+  },
+  { src: "https://source.unsplash.com/1600x900/?art", text: "art", id: 11 },
+  {
+    src: "https://source.unsplash.com/1600x900/?film",
+    text: "film",
+    id: 12,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?interiors",
+    text: "interiors",
+    id: 13,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?street-photography",
+    text: "street-photography",
+    id: 14,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?travel",
+    text: "travel",
+    id: 15,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?animals",
+    text: "animals",
+    id: 16,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?spirituality",
+    text: "spirituality",
+    id: 17,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?history",
+    text: "history",
+    id: 18,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?athletics",
+    text: "athletics",
+    id: 19,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?digital-screens",
+    text: "digital-screens",
+    id: 20,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?wallpapers",
+    text: "wallpapers",
+    id: 21,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?textures-patterns",
+    text: "textures-patterns",
+    id: 22,
+  },
+  { src: "https://source.unsplash.com/1600x900/?cars", text: "cars", id: 23 },
+  {
+    src: "https://source.unsplash.com/1600x900/?experimental",
+    text: "experimental",
+    id: 24,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?people",
+    text: "people",
+    id: 25,
+  },
+  { src: "https://source.unsplash.com/1600x900/?food", text: "food", id: 26 },
+  {
+    src: "https://source.unsplash.com/1600x900/?architecture",
+    text: "architecture",
+    id: 27,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?business-work",
+    text: "business-work",
+    id: 28,
+  },
+  {
+    src: "https://source.unsplash.com/1600x900/?fashion",
+    text: "fashion",
+    id: 29,
+  },
+  { src: "https://source.unsplash.com/1600x900/?art", text: "art", id: 30 },
+];
+
+export default imagelist;
